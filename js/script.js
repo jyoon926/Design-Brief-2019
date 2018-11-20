ed20e290f6edc2194595941af30747c865f853b9
Changhoon Yoon <changhoon.yoon@hstattler1-mp92.local>
2018-11-19 22:15:47 -0500

    Design update

--- js/script.js
+++ js/script.js
@@ -1,85 +1,42 @@
-window.addEventListener('mousemove', nutterbutter);
-window.addEventListener('mousemove', clicked);
+$(document).ready(function(){
 
-var _hoverB = document.getElementById('hoverB');
-var _hoverW = document.getElementById('hoverW');
+    //Smooth scrolling between links
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1000, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    //Parallax effect
+    if ( $(window).width()) {
+		var $man = $('.man');
+		var $win = $(window);
+		$win.on('scroll', function() {
+			var top1 = $win.scrollTop()/2;
+			$man.css('margin-top', '-' + top1 + 'px');
+		});
+	}
+});
+
+/*window.addEventListener('mousemove', cursor);
+var $win = $(window);
 var _cursor = document.getElementById('cursor');
-var _page = document.getElementById('about');
-var _clicked = false;
-var _link = "";
+var _cursorFollower = document.getElementById('cursorFollower');
 
 function everyTick() {
-    clicked();
+    cursor();
     setTimeout(arguments.callee, 0);
 }
 
-function nutterbutter() {
-    _hoverB.style.top = event.clientY - 90 + "px";
-	_hoverB.style.left = event.clientX - 90 + "px";
-    _hoverW.style.top = event.clientY - 90 + "px";
-	_hoverW.style.left = event.clientX - 90 + "px";
-    _cursor.style.top = event.clientY - 10 + "px";
-	_cursor.style.left = event.clientX - 10 + "px";
-}
-function hover() {
-    if (_clicked == false) {
-        _hoverB.style.transform = "scale(1.2)";
-        _cursor.style.opacity = "0";
-        _hoverW.style.transform = "scale(0)";
-    }
-    if (_clicked == true) {
-        _hoverW.style.transform = "scale(1.2)";
-        _cursor.style.opacity = "0";
-        _hoverB.style.transform = "scale(0)";
-    }
-}
-function out() {
-    _hoverB.style.transform = "scale(.2)";
-    _cursor.style.opacity = "1";
-}
-document.getElementById('aboutBtn').addEventListener('click',function(event) {
-    event.preventDefault();
-    _clicked = true;
-    _link = "about";
-});
-document.getElementById('albumBtn').addEventListener('click',function() {
-    event.preventDefault();
-    _clicked = true;
-    _link = "album";
-});
-document.getElementById('shopBtn').addEventListener('click',function() {
-    event.preventDefault();
-    _clicked = true;
-    _link = "shop";
-});
-document.getElementById('contactBtn').addEventListener('click',function() {
-    event.preventDefault();
-    _clicked = true;
-    _link = "contact";
-});
-
-function clicked() {
-    if (_clicked == true) {
-        if (_link == "about") { _page = document.getElementById('about'); }
-        if (_link == "album") { _page = document.getElementById('album'); }
-        if (_link == "shop") { _page = document.getElementById('shop'); }
-        if (_link == "contact") { _page = document.getElementById('contact'); }
-        _hoverB.style.transitionDuration = "1s";
-        _hoverB.style.transform = "scale(20)";
-        _cursor.style.opacity = "1";
-        _hoverW.style.transform = "scale(.2)";
-        document.getElementById("aboutBtn").style.opacity = "0";
-        document.getElementById("albumBtn").style.opacity = "0";
-        document.getElementById("shopBtn").style.opacity = "0";
-        document.getElementById("contactBtn").style.opacity = "0";
-        _page.style.transform = "scale(1)";
-        window.setTimeout(clickedFunc, 400);
-        function clickedFunc() {
-            document.getElementById("aboutBtn").style.display = "none";
-            document.getElementById("albumBtn").style.display = "none";
-            document.getElementById("shopBtn").style.display = "none";
-            document.getElementById("contactBtn").style.display = "none";
-            _page.style.opacity = "1";
-        }
-    }
-}
+function cursor() {
+    _cursor.style.top = event.clientY - 4 + $win.scrollTop() + "px";
+	_cursor.style.left = event.clientX - 4 + "px";
+    _cursorFollower.style.top = event.clientY - 17 + $win.scrollTop() + "px";
+	_cursorFollower.style.left = event.clientX - 17 + "px";
+}*/
