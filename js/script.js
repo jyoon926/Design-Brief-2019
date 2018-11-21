a43a9558a8849f8a6e10416c01b52d1052599907
Changhoon Yoon <changhoon.yoon@hstattler1-mp92.local>
2018-11-20 23:04:20 -0500

    Update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
+    $(window).on('beforeunload', function(){
+        $(window).scrollTop(0);
+    });
 
+    var overlay = document.getElementById("overlay");
+    var body = document.getElementById("body");
+    window.addEventListener('load', function(){
+        $('.bar').animate({ width: '100vw' }, 4000);
+        window.setTimeout(function () {
+            overlay.style.opacity = '0';
+            $('.loading').css("opacity", "0");
+            overlay.style.zIndex = '-100'; 
+        }, 4000);
+    })
     //Smooth scrolling between links
     $("a").on('click', function(event) {
         if (this.hash !== "") {
@@ -7,19 +20,22 @@ $(document).ready(function(){
             var hash = this.hash;
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 1000, function(){
+            }, 1200, function(){
                 window.location.hash = hash;
             });
         }
     });
 
     //Parallax effect
-    if ( $(window).width()) {
+    if ($(window).width() > 600) {
 		var $man = $('.man');
+		var $cross = $('.cross');
 		var $win = $(window);
 		$win.on('scroll', function() {
-			var top1 = $win.scrollTop()/2;
-			$man.css('margin-top', '-' + top1 + 'px');
+			var top1 = $win.scrollTop()/1.75;
+			var top2 = $win.scrollTop()/4;
+			$man.css('top', '-' + top1 + 'px');
+			$cross.css('margin-top', '-' + top2 + 'px');
 		});
 	}
 });
@@ -40,3 +56,6 @@ function cursor() {
     _cursorFollower.style.top = event.clientY - 17 + $win.scrollTop() + "px";
 	_cursorFollower.style.left = event.clientX - 17 + "px";
 }*/
+
+window.onload = function load() {
+}
